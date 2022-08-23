48a5fd103e49afcfc9e6750cbbf6e4ac7902e5eb
Martin Bojmaliev <mbojmaliev@gmail.com>
2022-08-23 13:39:49 +0200

    Installed Local_auth package

--- android/app/src/main/kotlin/com/example/password_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/password_manager/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.password_manager
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterFragmentActivity;
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine){
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
